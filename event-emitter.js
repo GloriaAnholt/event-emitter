c24b49450ed37a95b2e0779ed2bbafb5dc81f75f
Gloria Guy <totallygloria@gmail.com>
2017-03-24 16:18:58 -0700

    Adds passing addListener method
    addListener checks if the event is in the hash, if not it initializes
    an empty array to hold listeners. If the cb passed in is a function,
    it pushes it into the listeners array, else it returns false.

--- event-emitter.js
+++ event-emitter.js
@@ -4,6 +4,17 @@ class EventEmitter {
     constructor() {
         this.events = {};
     }
+
+    addListener(eventName, cb) {
+        if ( !this.events[eventName] ) {
+            this.events[eventName] = [];
+        }
+        if ( typeof cb === 'function' ) {
+            this.events[eventName].push(cb);
+        } 
+        else return false;
+    }
+    
 }
 
 module.exports = EventEmitter;
