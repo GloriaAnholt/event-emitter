e9381677560217509db4384242ce7125dbce460b
Gloria Guy <totallygloria@gmail.com>
2017-03-24 16:56:18 -0700

    Adds a test for the fail condition of passing a non-function to the addListener method.

--- test/event-emitter.test.js
+++ test/event-emitter.test.js
@@ -3,17 +3,26 @@ const EventEmitter = require('../event-emitter');
 
 describe('event emitter class', () => {
 
+    // create a global event emitter to use between tests
+    let ee = new EventEmitter();
+
+    // define a callback to register with events
+    let callback = function() {};
+
     it('creates an empty state object', () => {
-        let ee = new EventEmitter();
         assert.deepEqual(ee.events, {});
     }); 
 
     it('adds a new event type to the hash, registers handler fn', () => {
-        let ee = new EventEmitter();
-        let callback = function() {}
         ee.addListener('TEST', callback);
         assert.isOk(ee.events['TEST'])
         assert.deepEqual(ee.events['TEST'], [callback]);
-    })
+    });
+
+    it('returns false if you try to register a non-function', () => {
+        let response = ee.addListener('FAIL', 'string');
+        assert.isOk(ee.events['FAIL'])
+        assert.isFalse(response);
+    });
 
 });
